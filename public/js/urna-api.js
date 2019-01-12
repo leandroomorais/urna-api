d1aedf614e2fd297135f7030571e06d72de38beb
AntonioAlmeidaRego <38994121+AntonioAlmeidaRego@users.noreply.github.com>
2019-01-12 13:50:55 -0300

    Update na documentação dos servicos

--- public/js/urna-api.js
+++ public/js/urna-api.js
@@ -1,18 +1,19 @@
 function servicoTerminal(){
-	var result = {status: $("#status").val(), ipUrna: $("#id_ipUrna").val()};
+	var result = {status: $("#status").val(), ipUrna: $("#ipUrna1").val()};
 	var result1 = {};
 	
-	if(($("#status").val() == "liberada" || $("#status").val() == "bloqueada") && $("#id_ipUrna").val() != ""){
-		$('#resultado').html(JSON.stringify(result));	
-	}else{
-		$('#resultado').html(JSON.stringify(result1));	
-	}
+		if(($("#status").val() == "liberada" || $("#status").val() == "bloqueada") && ($("#ipUrna1").val() != "")){
+			$('#resultado').html(JSON.stringify(result));
+		}else{
+			$('#resultado').html(JSON.stringify(result1));	
+		}
+	
 }
 
 function servicoFinalizar(){
-	var result2 = {status: $("#finalizar").val(), ipUrna: $("#id_ipUrna").val()};
+	var result2 = {status: $("#finalizar").val(), ipUrna: $("#ipUrna2").val()};
 	var result1 = {};
-	if(($("#finalizar").val() == "true" || $("#finalizar").val() == "false") && $("#id_ipUrna").val() != "" ){
+	if(($("#finalizar").val() == "true" || $("#finalizar").val() == "false") && $("#ipUrna2").val() != "" ){
 		$('#resultado2').html(JSON.stringify(result2));		
 	}else{
 		$('#resultado2').html(JSON.stringify(result1));	
@@ -20,9 +21,9 @@ function servicoFinalizar(){
 }
 
 function servicoCancelar(){
-	var result3 = {status: $("#cancelar").val(), ipUrna: $("#id_ipUrna").val()};
+	var result3 = {status: $("#cancelar").val(), ipUrna: $("#ipUrna3").val()};
 	var result1 = {};
-	if(($("#cancelar").val() == "true" || $("#cancelar").val() == "false") && $("#id_ipUrna").val() != ""){
+	if(($("#cancelar").val() == "true" || $("#cancelar").val() == "false") && $("#ipUrna3").val() != ""){
 		$('#resultado3').html(JSON.stringify(result3));
 	}else{
 		$('#resultado3').html(JSON.stringify(result1));	
@@ -30,14 +31,14 @@ function servicoCancelar(){
 }
 
 function servicoSecao(){
-	var result4 = {secao: $("#secao").val(), ip: $("#ipTerminal").val(), ipUrna: $("#id_ipUrna").val()};
-	if($("#secao").val() != "" && $("#ipTerminal").val() != "" && ipUrna: $("#id_ipUrna").val() != ""){
+	var result4 = {secao: $("#secao").val(), ip: $("#ipTerminal").val(), ipUrna: $("#ipUrna5").val()};
+	if($("#secao").val() != "" && $("#ipTerminal").val() != "" && $("#ipUrna5").val() != ""){
 		$('#resultado4').html(JSON.stringify(result4));	
 	}
 }
 
 function servicoBoletim(){
-	$.getJSON("https://urna-api.herokuapp.com/api/boletim/"+$("#ipUrna").val()).done(function(data){
+	$.getJSON("https://urna-api.herokuapp.com/api/boletim/"+$("#ipUrna4").val()).done(function(data){
 		$("#resultadoIPUrna").text(JSON.stringify(data));
 	});
 }
